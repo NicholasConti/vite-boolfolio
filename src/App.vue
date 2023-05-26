af6428cf80dfbcdcd3e1bd84c7aa0da556c42c83
NicholasConti <conti94@live.it>
2023-05-26 16:17:18 +0200

    sett vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,38 @@
 <script>
-import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
-import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
 
 export default {
     name: 'App',
-    data() {
-        return {
-            apiBaseUrl: 'http://127.0.0.1:8000/api',
-            apiUrls: {
-                projects: '/projects'
-            },
-            projects: []
-        }
-    },
+    // data() {
+    //     return {
+    //         apiBaseUrl: 'http://127.0.0.1:8000/api',
+    //         apiUrls: {
+    //             projects: '/projects'
+    //         },
+    //         projects: []
+    //     }
+    // },
     components: {
         AppHeader,
-        AppMain,
         AppFooter
     },
-    methods: {
-        getProjects() {
-            axios.get(this.apiBaseUrl + this.apiUrls.projects).then((response) => {
-                this.projects = response.data.results;
-            })
-        }
-    },
-    created() {
-        this.getProjects();
-    }
+    // methods: {
+    //     getProjects() {
+    //         axios.get(this.apiBaseUrl + this.apiUrls.projects).then((response) => {
+    //             this.projects = response.data.results;
+    //         })
+    //     }
+    // },
+    // created() {
+    //     this.getProjects();
+    // }
 }
 </script>
 
 <template>
     <AppHeader />
-    <AppMain :data="projects" />
+    <router-view></router-view>
     <AppFooter />
 </template>
 

f3ccc5179eec9c7c32ca4d5527bb91abb857acf9
NicholasConti <conti94@live.it>
2023-05-26 18:00:26 +0200

    try redirect with errors

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+    <section>
+        <h1>PAGINA NON TROVATA</h1>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'NotFound'
+}
+</script>
+
+<style lang="scss" scoped></style>

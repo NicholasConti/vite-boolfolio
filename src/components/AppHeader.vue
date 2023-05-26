ee24fc754e473306849e1870d99aa25ca39b4d5a
NicholasConti <conti94@live.it>
2023-05-26 16:17:30 +0200

    add links w/vue router

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,7 +4,23 @@
             <div class="d-flex justify-content-between">
                 <div>Logo</div>
                 <nav>
-                    <div>Home</div>
+                    <ul class="list-unstyled m-0 p-0 gap-2 d-flex">
+                        <li>
+                            <router-link :to="{ name: 'home' }" class="nav-link">
+                                Home
+                            </router-link>
+                        </li>
+                        <li>
+                            <router-link :to="{ name: 'about' }" class="nav-link">
+                                About
+                            </router-link>
+                        </li>
+                        <li>
+                            <router-link :to="{ name: 'projects' }" class="nav-link">
+                                Projects
+                            </router-link>
+                        </li>
+                    </ul>
                 </nav>
             </div>
         </div>
